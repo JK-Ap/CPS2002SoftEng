19a4de349ea7ff29cf0f4f3fecfc74656660a6a6
Jean Karl Apap <jean-karl.apap.14@um.edu.mt>
2016-02-29 09:08:56 +0100

    Edited Divide.

--- src/Divide.java
+++ src/Divide.java
@@ -2,6 +2,16 @@ public class Divide implements Operation
 {
 	public int perform(int x,y) //performs operation on x,y
 	{
+		int err = 0;
+		
+		if(y == 0) {
+			
+			System.out.println("Error occured !");
+			err = -999;
+			
+			return err;
+		}
+		
 		return x/y;
 	}
 }
