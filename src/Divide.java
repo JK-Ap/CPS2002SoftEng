db7ff968db41b5503e5fd8cc3f6b2f8af9e0b5c8
Michael Calleja <michaelcalleja95@gmail.com>
2016-02-29 09:09:42 +0100

    exception for divide by 0

--- src/Divide.java
+++ src/Divide.java
@@ -2,6 +2,14 @@ public class Divide implements Operation
 {
 	public int perform(int x,y) //performs operation on x,y
 	{
-		return x/y;
+		if (y == 0) 
+		{
+			throw new ArithmeticException("denominator == 0!");
+			return -1;
+		}
+		else
+		{
+			return x/y;
+		}
 	}
 }
